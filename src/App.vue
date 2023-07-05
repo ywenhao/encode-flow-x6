e697abf587881b145e60c44b34989f72f46c78f2
actions.win <actions.win@foxmail.com>
2023-07-05 20:15:53 +0800

    feat(app): 完善实例

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { onMounted, ref, watch } from 'vue'
 import { useFlow } from './useFlow'
 import { store } from './store'
 import { addStartNode } from './utils'
+import type { NodeData } from './types'
 
 const containerRef = ref<HTMLDivElement>()
 const { graphRef } = useFlow(containerRef)
@@ -16,7 +17,17 @@ onMounted(() => {
 watch(
   () => store.selectNode,
   (node) => {
-    console.log('selectNode', node)
+    const json = graphRef.value!.toJSON()
+    const str = JSON.stringify(json)
+    console.warn(str)
+    const data = node!.getData<NodeData>()
+    setTimeout(() => {
+      graphRef.value?.clearCells()
+      setTimeout(() => {
+        graphRef.value?.fromJSON(JSON.parse(str))
+      }, 1000)
+    }, 1000)
+    console.warn('selectNode-data', data)
   },
 )
 </script>
